6c20659159237dfd6d25fd6b70fa9376249ff524
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2023-08-22 08:05:24 +0100

    Create function playRound
    The function playRound takes the user's choice and the the computer's choice, compares them to each other, and returns a string explaining the outcome.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function capitalise(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
 function getComputerChoice() {
 	randNum = Math.floor(Math.random() * 3);
 
@@ -7,5 +11,18 @@ function getComputerChoice() {
 		return "paper";
 	} else {
 		return "scissors";
+	}
+}
+
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection == computerSelection) {
+		return `You drew! You both chose ${capitalise(playerSelection)}.`
+	}
+	else if ((playerSelection == "rock" && computerSelection == "paper")
+	|| (playerSelection == "paper" && computerSelection == "scissors")
+	|| (playerSelection == "scissors" && computerSelection == "rock")) {
+		return `You lost! ${capitalise(computerSelection)} beats ${capitalise(playerSelection)}.`
+	} else {
+		return `You won! ${capitalise(playerSelection)} beats ${capitalise(computerSelection)}.`
 	}
 }
