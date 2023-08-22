07e39f6f9da34995e06b38c9fe66be50517020db
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2023-08-22 08:51:54 +0100

    Create function game
    game() calls playRound five times and prints round number and outcome to the console and says who wins after the five rounds.

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+let playerRoundsWon = 0;
+let computerRoundsWon = 0;
+let roundsDrawn = 0;
+
 function capitalise(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
 function getComputerChoice() {
-	randNum = Math.floor(Math.random() * 3);
+	let randNum = Math.floor(Math.random() * 3);
 
 	if (randNum == 0) {
 		return "rock";
@@ -16,13 +20,38 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection == computerSelection) {
-		return `You drew! You both chose ${capitalise(playerSelection)}.`
+		roundsDrawn++;
+		return `You drew! You both chose ${capitalise(playerSelection)}.`;
 	}
 	else if ((playerSelection == "rock" && computerSelection == "paper")
 	|| (playerSelection == "paper" && computerSelection == "scissors")
 	|| (playerSelection == "scissors" && computerSelection == "rock")) {
-		return `You lost! ${capitalise(computerSelection)} beats ${capitalise(playerSelection)}.`
+		computerRoundsWon++;
+		return `You lost! ${capitalise(computerSelection)} beats ${capitalise(playerSelection)}.`;
 	} else {
-		return `You won! ${capitalise(playerSelection)} beats ${capitalise(computerSelection)}.`
+		playerRoundsWon++;
+		return `You won! ${capitalise(playerSelection)} beats ${capitalise(computerSelection)}.`;
 	}
 }
+
+function game() {
+	console.log("Welcome to Rock, Paper, Scissors! You will be playing five rounds against a computer. Let's see who wins!");
+	for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
+		if (roundsPlayed != 4) {
+			console.log(`Round ${roundsPlayed + 1}`);
+		} else {
+			console.log("Final Round!");
+		}
+		let playerSelection = prompt("Pick Rock, Paper, or Scissors").toLowerCase();
+
+		console.log(playRound(playerSelection, getComputerChoice()));
+	}
+
+	if (playerRoundsWon > computerRoundsWon) {
+		console.log(`Well Done! You beat the computer! You won ${playerRoundsWon}, drew ${roundsDrawn} and lost ${computerRoundsWon}!`)
+	} else if (playerRoundsWon < computerRoundsWon) {
+		console.log(`Oh no! The computer beat you! You won ${playerRoundsWon}, drew ${roundsDrawn} and lost ${computerRoundsWon}.`)
+	}
+}
+
+game()
