d85895435e0f74059a4023074b838aa481357147
RobbieS-coder <127651345+RobbieS-coder@users.noreply.github.com>
2023-09-03 08:46:06 +0100

    Change console.logs to DOM methods
    The code is no longer reliant on the console and one round can be played as many times as the user wants. Displaying score and announcing the winner is yet to come.

--- script.js
+++ script.js
@@ -1,113 +1,94 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let playerRoundsWon = 0;
-  let computerRoundsWon = 0;
-  let roundsDrawn = 0;
-  let playerChoice;
+const roundNumber = document.querySelector("#round-number");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const msg = document.querySelector("#msg");
+const score = document.querySelector("#score");
 
-  function capitalise(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+let playerRoundsWon = 0;
+let computerRoundsWon = 0;
+let roundsDrawn = 0;
+let roundResult;
+
+function capitalise(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function getComputerChoice() {
+  let randNum = Math.floor(Math.random() * 3);
+
+  if (randNum == 0) {
+    return "rock";
+  } else if (randNum == 1) {
+    return "paper";
+  } else {
+    return "scissors";
   }
+}
 
-  function getComputerChoice() {
-    let randNum = Math.floor(Math.random() * 3);
+function getPlayerChoice() {
+  rock.addEventListener("click", () => {
+    roundResult = playRound("rock", getComputerChoice());
+    displayResult();
+  });
+  paper.addEventListener("click", () => {
+    roundResult = playRound("paper", getComputerChoice());
+    displayResult();
+  });
+  scissors.addEventListener("click", () => {
+    roundResult = playRound("scissors", getComputerChoice());
+    displayResult();
+  });
+}
 
-    if (randNum == 0) {
-      return "rock";
-    } else if (randNum == 1) {
-      return "paper";
-    } else {
-      return "scissors";
-    }
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    roundsDrawn++;
+    return `You drew! You both chose ${capitalise(playerSelection)}.`;
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "rock")
+  ) {
+    computerRoundsWon++;
+    return `You lost! ${capitalise(computerSelection)} beats ${capitalise(
+      playerSelection
+    )}.`;
+  } else {
+    playerRoundsWon++;
+    return `You won! ${capitalise(playerSelection)} beats ${capitalise(
+      computerSelection
+    )}.`;
   }
+}
 
-  function getPlayerChoice() {
-    const rock = document.querySelector("#rock");
-    const paper = document.querySelector("#paper");
-    const scissors = document.querySelector("#scissors");
+function displayResult() {
+  msg.textContent = roundResult;
+}
 
-    rock.addEventListener("click", (e) => {
-      playerChoice = "rock";
-      game();
-    });
-    paper.addEventListener("click", (e) => {
-      playerChoice = "paper";
-      game();
-    });
-    scissors.addEventListener("click", (e) => {
-      playerChoice = "scissors";
-      game();
-    });
+function endGame() {
+  if (playerRoundsWon > computerRoundsWon) {
+    msg.textContent = "Well Done! You beat the computer!";
+  } else if (playerRoundsWon < computerRoundsWon) {
+    msg.textContent = "Oh no! The computer beat you!";
   }
+  playAgain();
+}
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-      roundsDrawn++;
-      return `You drew! You both chose ${capitalise(playerSelection)}.`;
-    } else if (
-      (playerSelection == "rock" && computerSelection == "paper") ||
-      (playerSelection == "paper" && computerSelection == "scissors") ||
-      (playerSelection == "scissors" && computerSelection == "rock")
-    ) {
-      computerRoundsWon++;
-      return `You lost! ${capitalise(computerSelection)} beats ${capitalise(
-        playerSelection
-      )}.`;
-    } else {
-      playerRoundsWon++;
-      return `You won! ${capitalise(playerSelection)} beats ${capitalise(
-        computerSelection
-      )}.`;
-    }
+function playAgain() {
+  let playAgainChoice = prompt("Do you want to play again? (y/n)");
+
+  if (playAgainChoice == "y") {
+    playerRoundsWon = 0;
+    computerRoundsWon = 0;
+    roundsDrawn = 0;
+    // Reset
+  } else if (playAgainChoice == "n") {
+    msg.textContent = "Thank you for playing!";
+    return;
   }
+}
 
-  function game() {
-    console.log(
-      "Welcome to Rock, Paper, Scissors! You will be playing five rounds against a computer. Let's see who wins!"
-    );
-
-    getPlayerChoice();
-    console.log(playRound(playerChoice, getComputerChoice()));
-
-    endGame();
-  }
-
-  function endGame() {
-    if (playerRoundsWon > computerRoundsWon) {
-      console.log(
-        `Well Done! You beat the computer! You won ${playerRoundsWon}, drew ${roundsDrawn} and lost ${computerRoundsWon}!`
-      );
-    } else if (playerRoundsWon < computerRoundsWon) {
-      console.log(
-        `Oh no! The computer beat you! You won ${playerRoundsWon}, drew ${roundsDrawn} and lost ${computerRoundsWon}.`
-      );
-    } else {
-      console.log(
-        `It's a tie! You won ${playerRoundsWon}, drew ${roundsDrawn} and lost ${computerRoundsWon}. Play one last round as a tie-breaker!`
-      );
-      console.log("Tie-breaker!");
-
-      console.log(playRound(playerChoice, getComputerChoice()));
-      endGame();
-    }
-    playAgain();
-  }
-
-  function playAgain() {
-    let playAgainChoice = prompt("Do you want to play again? (y/n)");
-
-    if (playAgainChoice == "y") {
-      playerRoundsWon = 0;
-      computerRoundsWon = 0;
-      roundsDrawn = 0;
-      game();
-    } else if (playAgainChoice == "n") {
-      console.log("Thank you for playing!");
-      return;
-    } else {
-      console.log(`${playAgainChoice} is invalid choice.`);
-      playAgain();
-    }
-  }
-
-  getPlayerChoice();
-});
+msg.textContent =
+  "Welcome to Rock, Paper, Scissors! You will be playing the first to five rounds against a computer. Let's see who wins!";
+getPlayerChoice();
